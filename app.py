ee50849b61d067c1b06f28e14d79e687b019cd22
unknown <jnjw55@gmail.com>
2023-05-20 10:11:28 +0800

    Auto-commit at 2023-05-20 10:11:28

--- app.py
+++ app.py
@@ -1,47 +1,31 @@
-import os
-
-import telebot
-
-from utils import get_daily_horoscope
+from telegram.ext import CommandHandler, Updater
+from logging import basicConfig, getLogger, INFO
 
 BOT_TOKEN = "6207929876:AAGLcN0irRbfZFi27b7jFEDJfUprStJPy6M"
 
-bot = telebot.TeleBot(BOT_TOKEN)
+basicConfig(level=INFO)
+log = getLogger()
 
+def start(update, context):
+    update.message.reply_text(
+        "start this bot",
+        parse_mode="markdown")
 
-@bot.message_handler(commands=['start', 'hello'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+def help(update, context):
+    update.message.reply_text(
+        "help for this bot",
+        parse_mode="markdown")
 
+def main():
+    updater = Updater(token=BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
 
-@bot.message_handler(commands=['horoscope'])
-def sign_handler(message):
-    text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
-    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
-    bot.register_next_step_handler(sent_msg, day_handler)
+    start_handler = CommandHandler("start", start)
+    help_handler = CommandHandler("help", help)
 
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler)
+    updater.start_polling()
 
-def day_handler(message):
-    sign = message.text
-    text = "What day do you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
-    sent_msg = bot.send_message(
-        message.chat.id, text, parse_mode="Markdown")
-    bot.register_next_step_handler(
-        sent_msg, fetch_horoscope, sign.capitalize())
-
-
-def fetch_horoscope(message, sign):
-    day = message.text
-    horoscope = get_daily_horoscope(sign, day)
-    data = horoscope["data"]
-    horoscope_message = f'*Horoscope:* {data["horoscope_data"]}\n*Sign:* {sign}\n*Day:* {data["date"]}'
-    bot.send_message(message.chat.id, "Here's your horoscope!")
-    bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
-
-
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
-
-bot.infinity_polling()
+if __name__ == '__main__':
+    main()
